8612a714ad217655a296e8c481b2774886c02765
LLS <2145604059@qq.com>
2019-02-21 09:57:58 +0800

    20192/2/21

---
+++ 引入Properties文件/src/main/java/priv/lint/introducingpropertiesfiles/IntroducingpropertiesfilesApplication.java
@@ -0,0 +1,13 @@
+package priv.lint.introducingpropertiesfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducingpropertiesfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroducingpropertiesfilesApplication.class, args);
+    }
+
+}
---
+++ 引入Properties文件/src/test/java/priv/lint/introducingpropertiesfiles/IntroducingpropertiesfilesApplicationTests.java
@@ -0,0 +1,16 @@
+package priv.lint.introducingpropertiesfiles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntroducingpropertiesfilesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
